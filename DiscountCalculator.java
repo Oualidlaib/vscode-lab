b52a04803ee67e9bf409d8d6272b1ac2bb7b2fc6
Oualidlaib <laiboualid2003@gmail.com>
2026-04-27 17:44:33 +0100

    add DiscountCalculator

---
+++ DiscountCalculator.java
@@ -0,0 +1,10 @@
+class DiscountCalculator {
+    public double calculateTotal(double subtotal, double taxRate) {
+        double tax = subtotal * taxRate;
+        return subtotal + tax;
+    }
+
+    public double applyDiscount(double total, double discountRate) {
+        return total * (1 - discountRate);
+    }
+}
