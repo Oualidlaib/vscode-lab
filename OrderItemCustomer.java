89501cf770ba37aeadc3984faed97054a36e6372
Oualidlaib <laiboualid2003@gmail.com>
2026-04-27 15:48:59 +0100

    add Order, Item, and Customer

---
+++ OrderItemCustomer.java
@@ -0,0 +1,43 @@
+class Order {
+    private Customer customer;
+    private List<Item> items;
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+}
+
+class Customer {
+    private String name;
+    private boolean isMember;
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isMember() {
+        return isMember;
+    }
+}
+
+class Item {
+    private String name;
+    private double price;
+    private int quantity;
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
