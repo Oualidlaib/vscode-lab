dc22e810abb3382fe49545013aaa2bc2584c6a1d
Oualidlaib <laiboualid2003@gmail.com>
2026-04-27 17:32:16 +0100

    add Dog and Cat (before refactoring)

---
+++ DogCat.java
@@ -0,0 +1,19 @@
+class Dog {
+    public void eat() {
+        System.out.println("Dog is eating");
+    }
+
+    public void sleep() {
+        System.out.println("Dog is sleeping");
+    }
+}
+
+class Cat {
+    public void eat() {
+        System.out.println("Cat is eating");
+    }
+
+    public void sleep() {
+        System.out.println("Cat is sleeping");
+    }
+}
